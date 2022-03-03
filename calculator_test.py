6301773ba6113e77d5579768b67b882534e9d5ad
ahmed morgan <ahmed555morgan@gmail.com>
2022-03-03 05:25:37 -0800

    ahmed test2

--- calculator_test.py
+++ calculator_test.py
@@ -16,7 +16,7 @@ class Test(unittest.TestCase):
         self.assertEqual(result,16)
     def test_0_divide(self):
         result = self.calculator.divide(4,4)
-        self.assertEqual(result,16)
+        self.assertEqual(result,1)
 if __name__ == '__main__':
     # begin the unittest.main()
     unittest.main()
